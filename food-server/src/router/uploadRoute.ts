82edf37688ea2c984640f59f63482fddd9683eb4
Билгүүн Гантогос <LP230023@G301-CC0190.local>
2024-03-20 03:33:29 -0700

    multer zasav

--- food-server/src/router/uploadRoute.ts
+++ food-server/src/router/uploadRoute.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { uploadFile } from "../controller/uploadController";
-import multer from "multer";
+import { upload } from "../utils/multer";
 
 export const uploadRoute = Router();
-const upload = multer({ dest: "./uploads" });
 
 uploadRoute.route("/").post(upload.single("image"), uploadFile);
--- food-server/src/utils/multer.ts
+++ food-server/src/utils/multer.ts
@@ -1,3 +1,15 @@
 import multer from "multer";
+import path from "path";
 
-export const upload = multer({ dest: "./uploads" });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log("d", path.join(__dirname, "../uploads"));
+
+    cb(null, path.join(__dirname, "../uploads/"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage });
